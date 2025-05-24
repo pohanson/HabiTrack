6bd6b33e340de40ecb0ec2702fb32a6c45aa68c1
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-05-24 20:01:58 +0800

    Add FloatingActionButton

---
+++ HabiTrack/components/FloatingActionButton.tsx
@@ -0,0 +1,40 @@
+import { Pressable, StyleSheet, useColorScheme, View } from 'react-native';
+import { IconSymbol, IconSymbolName } from './ui/IconSymbol';
+import { Colors } from '@/constants/Colors';
+export function FloatingActionButton({
+  onPress,
+  iconName,
+  color,
+}: {
+  onPress: () => void;
+  iconName: IconSymbolName;
+  color?: string;
+}) {
+  const colorScheme = useColorScheme() || 'light';
+  const backgroundColor = color || Colors[colorScheme].tint;
+  return (
+    <View style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <Pressable
+        onPress={onPress}
+        style={(isPressed) => [
+          {
+            backgroundColor: backgroundColor,
+            opacity: isPressed.pressed ? 0.7 : 1,
+            position: 'absolute',
+            alignSelf: 'flex-end',
+          },
+          styles.fab,
+        ]}>
+        <IconSymbol name={iconName} color={'#FFFFFF'} />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  fab: {
+    elevation: 6,
+    borderRadius: '50%',
+    padding: 10,
+  },
+});
