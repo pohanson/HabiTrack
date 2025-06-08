eade19c48df1699f373459471482d92d9624b172
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-06-08 22:53:43 +0800

    Habit Completion Day and Time

---
+++ HabiTrack/types/DaysOfWeek.ts
@@ -0,0 +1,20 @@
+export enum DaysOfWeek {
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
+  Sunday = 'Sunday',
+}
+
+// based on js Date.getDay() where 0=Sunday, ..., 6=Saturday
+export const daysOfWeekArray: DaysOfWeek[] = [
+  DaysOfWeek.Sunday,
+  DaysOfWeek.Monday,
+  DaysOfWeek.Tuesday,
+  DaysOfWeek.Wednesday,
+  DaysOfWeek.Thursday,
+  DaysOfWeek.Friday,
+  DaysOfWeek.Saturday,
+];
