8a1310bc9adc570e4a090e39d23cd7334f9077ce
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-06-10 23:43:28 +0800

    Remove Extra Files

--- HabiTrack/app/(tabs)/_layout.tsx
+++ HabiTrack/app/(tabs)/_layout.tsx
@@ -2,9 +2,7 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -16,8 +14,6 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: true,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             position: 'absolute',
--- HabiTrack/app/(tabs)/index.tsx
+++ HabiTrack/app/(tabs)/index.tsx
@@ -1,8 +1,7 @@
 import { Image } from 'expo-image';
-import { StyleSheet, TouchableOpacity, useColorScheme, View } from 'react-native';
+import { ScrollView, StyleSheet, TouchableOpacity, useColorScheme, View } from 'react-native';
 
 import { FloatingActionButton } from '@/components/FloatingActionButton';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { router, useFocusEffect } from 'expo-router';
@@ -56,14 +55,7 @@ export default function HomeScreen() {
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+    <ThemedView style={{ flex: 1, backgroundColor: colors.background }}>
       <ThemedView
         style={{
           width: '100%',
@@ -83,59 +75,65 @@ export default function HomeScreen() {
           }}>
           <IconSymbol name="list.bullet" color="white" />
         </TouchableOpacity>
-        <ThemedText type="title" style={{ fontSize: 26 }}>{`Today's Habits`}</ThemedText>
+        <ThemedText type="title" style={{ fontSize: 26 }}>
+          Today's Habits
+        </ThemedText>
         <FloatingActionButton iconName="plus" onPress={() => router.push('/habit/create')} />
       </ThemedView>
       {data?.length === 0 && <ThemedText>No habits for today.</ThemedText>}
-      {data.map(({ habit, habit_completion }, idx) => (
-        <View
-          key={idx}
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: colors.background,
-            borderColor: colors.border,
-            borderWidth: 2,
-            borderRadius: 15,
-            padding: 10,
-          }}>
-          <ThemedText style={{ flex: 1, fontWeight: 'bold' }}>{habit.name}</ThemedText>
-          <TouchableOpacity onPress={() => EditButtonPressed(habit.id)}>
-            <Image
-              source={require('@/assets/images/edit-icon.png')}
-              style={{ width: 24, height: 24 }}
-            />
-          </TouchableOpacity>
-          <ToggleInput
-            label=""
-            iconName="checkmark"
-            selected={habit_completion !== null}
-            iconSize={24}
-            toggleSelected={function (): void {
-              if (habit_completion === null) {
-                // habit is not completed today, so add completion
-                drizzleDb
-                  .insert(habitCompletion)
-                  .values({
-                    habit_id: habit.id,
-                    completedAt: today,
-                  })
-                  .execute();
-              } else {
-                // habit is already completed today, remove completion
-                drizzleDb
-                  .delete(habitCompletion)
-                  .where(eq(habitCompletion.id, habit_completion.id))
-                  .execute();
-              }
-              setRefresh((prev) => prev + 1);
-            }}
-          />
-        </View>
-      ))}
-    </ParallaxScrollView>
+      <ScrollView>
+        <ThemedView style={{ gap: 10, padding: 10 }}>
+          {data.map(({ habit, habit_completion }, idx) => (
+            <View
+              key={idx}
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                backgroundColor: colors.background,
+                borderColor: colors.border,
+                borderWidth: 2,
+                borderRadius: 15,
+                padding: 10,
+              }}>
+              <ThemedText style={{ flex: 1, fontWeight: 'bold' }}>{habit.name}</ThemedText>
+              <TouchableOpacity onPress={() => EditButtonPressed(habit.id)}>
+                <Image
+                  source={require('@/assets/images/edit-icon.png')}
+                  style={{ width: 24, height: 24 }}
+                />
+              </TouchableOpacity>
+              <ToggleInput
+                label=""
+                iconName="checkmark"
+                selected={habit_completion !== null}
+                iconSize={24}
+                toggleSelected={function (): void {
+                  if (habit_completion === null) {
+                    // habit is not completed today, so add completion
+                    drizzleDb
+                      .insert(habitCompletion)
+                      .values({
+                        habit_id: habit.id,
+                        completedAt: today,
+                      })
+                      .execute();
+                  } else {
+                    // habit is already completed today, remove completion
+                    drizzleDb
+                      .delete(habitCompletion)
+                      .where(eq(habitCompletion.id, habit_completion.id))
+                      .execute();
+                  }
+                  setRefresh((prev) => prev + 1);
+                }}
+              />
+            </View>
+          ))}
+        </ThemedView>
+      </ScrollView>
+    </ThemedView>
   );
 }
 
@@ -143,17 +141,7 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    padding: 14,
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
   },
 });
