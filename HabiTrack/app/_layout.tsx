a2861d2b1dbfe83917d2c9ce91c6ae7c02dfd1ae
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-06-16 17:23:44 +0800

    Setting up Notification

--- HabiTrack/app/_layout.tsx
+++ HabiTrack/app/_layout.tsx
@@ -10,19 +10,28 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { SQLiteProvider, openDatabaseSync } from 'expo-sqlite';
-import React, { Suspense } from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { Suspense, useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  registerForPushNotificationsAsync,
+  schedulePushNotification,
+} from '@/services/notification';
+import { habit, reminder } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 export default function RootLayout() {
-  const expoDb = openDatabaseSync(DATABASE_NAME);
-  const db = drizzle(expoDb);
-  const { error } = useMigrations(db, migrations);
+  useEffect(() => {
+    registerForPushNotificationsAsync().catch(console.log);
+  }, []);
 
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const expoDb = openDatabaseSync(DATABASE_NAME);
+  const db = drizzle(expoDb);
+  const { error } = useMigrations(db, migrations);
 
   if (error) {
     console.log('Database migration failed:', error);
@@ -33,6 +42,24 @@ export default function RootLayout() {
     // Async font loading only occurs in development.
     return null;
   }
+  db.select({ reminder: reminder, habit: habit })
+    .from(reminder)
+    .innerJoin(habit, eq(reminder.habit_id, habit.id))
+    .where(eq(reminder.day, new Date().getDay()))
+    .then((data) => {
+      for (const { reminder, habit } of data) {
+        let hour, minute;
+        if (reminder.time == null) {
+          hour = 9; // Default hour
+          minute = 0; // Default minute
+        } else {
+          hour = parseInt(reminder.time.slice(0, 2));
+          minute = parseInt(reminder.time.slice(2, 4));
+        }
+        schedulePushNotification(reminder.day, hour, minute, habit?.name, habit?.description || '');
+      }
+    })
+    .catch(console.error);
 
   return (
     <SafeAreaProvider>
